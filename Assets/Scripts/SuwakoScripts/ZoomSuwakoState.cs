0a7c94ce1bf865839794ad63ac29de0a9b4d7666
RedRaccoon177 <145746284+RedRaccoon177@users.noreply.github.com>
2025-01-22 20:55:11 +0900

    [Feat] 보스 등장 ui 구현

---
+++ Assets/Scripts/SuwakoScripts/ZoomSuwakoState.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomSuwakoState : SuwakoState
+{
+    bool startShow = true;
+
+    public GameObject ui;
+    public float fadeDuration = 1f; // 페이드 시간 (초)
+
+    public override void Enter(SuwakoController suwako)
+    {
+        // SpriteRenderer 가져오기
+        ui = GameObject.Find("SuwakoImage");
+        ui.SetActive(false);
+        Camera.main.GetComponent<CameraMove>().ZoomBoss(suwako.gameObject, startShow);
+        suwako.StartCoroutine(ShowUIStart(suwako));
+    }
+
+    IEnumerator ShowUIStart(SuwakoController suwako)
+    {
+        yield return new WaitForSeconds(1.2f);
+        ui.SetActive(true);
+
+        yield return new WaitForSeconds(3.5f);
+        ui.SetActive(false);
+
+        startShow = false;
+        Camera.main.GetComponent<CameraMove>().ZoomBoss(suwako.gameObject, startShow);
+    }
+}
