74eeacc76b2df32d0b5026d65da78e4cf0672935
RedRaccoon177 <145746284+RedRaccoon177@users.noreply.github.com>
2025-01-22 11:35:47 +0900

    [Feat] 스와코 패턴 구현
    스와코의 모든 패턴을 구현하였다. 각 패턴에 알맞는 행동 패턴 또한 구현하였다.

--- Assets/Scripts/SuwakoScripts/SuwakoWeakPointState.cs
+++ Assets/Scripts/SuwakoScripts/SuwakoWeakPointState.cs
@@ -5,18 +5,11 @@ using UnityEngine.UI;
 
 public class SuwakoWeakPointState : SuwakoState
 {
-
-
-    //타격 받을 경우 색 변환 => 점점 어두워짐.
-
-    //4대 이상 얻어 맞으면 다시 원래 상태로 돌아옴.
-
     public float maxHealth; // 최대 체력
     private float damage;   // 데미지 받았을 때
 
-    float usingTime = 10f; // 전환 시간 (초)
+    float usingTime = 6f; // 전환 시간 (초)
     float endTime = 0;
-
     int hitCount = 0; 
 
     public override void Enter(SuwakoController suwako)
@@ -41,15 +34,9 @@ public class SuwakoWeakPointState : SuwakoState
     {
         if(Time.time > endTime)
         {
-            suwako.animator.SetBool("IsWeakPoint", false);
-            suwako.boxCollider.enabled = true;
-            suwako.weakPointCollider.enabled = false;
-            suwako.rb.bodyType = RigidbodyType2D.Dynamic;
-            suwako.childObjects[8].SetActive(false);
-            suwako.ChangeState(suwako.idleState);
+            ReturnIdle(suwako);
         }
     }
-
     public override void OnCollisionEnter2D(SuwakoController suwako, Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -60,12 +47,7 @@ public class SuwakoWeakPointState : SuwakoState
             }
             else if (hitCount >= 4)
             {
-                suwako.animator.SetBool("IsWeakPoint", false);
-                suwako.boxCollider.enabled = true;
-                suwako.weakPointCollider.enabled = false;
-                suwako.rb.bodyType = RigidbodyType2D.Dynamic;
-                suwako.childObjects[8].SetActive(false);
-                suwako.ChangeState(suwako.idleState);
+                ReturnIdle(suwako);
             }
         }
     }
@@ -74,13 +56,26 @@ public class SuwakoWeakPointState : SuwakoState
     {
         hitCount++;
         suwako.suwakoHP -= damage;
-        suwako.suwakoHP = Mathf.Clamp(suwako.suwakoHP, 0, maxHealth); // 체력을 0~최대값 사이로 제한
+        suwako.suwakoHP = Mathf.Clamp(suwako.suwakoHP, 0, maxHealth); // 체력을 0 ~ 최대값 사이로 제한
+        
         //테스트용 승리
         if (suwako.suwakoHP <= 0)
         {
             suwako.suwakoHP = 0;
+            suwako.animator.SetTrigger("IsDead");
+            suwako.rb.isKinematic = true;
             GameManager.Instance.Victory();
         }
         suwako.GetComponent<MonsterHPObserver>().NotifyHealthChange(maxHealth, suwako.suwakoHP);
+    }
+
+    void ReturnIdle(SuwakoController suwako)
+    {
+        suwako.animator.SetBool("IsWeakPoint", false);
+        suwako.boxCollider.enabled = true;
+        suwako.weakPointCollider.enabled = false;
+        suwako.rb.bodyType = RigidbodyType2D.Dynamic;
+        suwako.childObjects[8].SetActive(false);
+        suwako.ChangeState(suwako.idleState);
     }
 }
