fd654014d74076b892980a9c66408af4c4f989dc
Fifow1 <lasldjf123@naver.com>
2025-01-23 10:19:16 +0900

    좀비 프리펩 레이어 수정

--- Assets/Scripts/Zombie/ZombieDie.cs
+++ Assets/Scripts/Zombie/ZombieDie.cs
@@ -16,7 +16,7 @@ public class ZombieDie : ZombieState
             zombie.transform.position = Vector3.zero;
             zombie.transform.rotation = Quaternion.identity;
             Camera.main.GetComponent<CameraMove>().ZoomBoss(zombie.gameObject,true);
-            //GameManager.Instance.Victory();
+            GameManager.Instance.Victory();
         }
     }
 }
